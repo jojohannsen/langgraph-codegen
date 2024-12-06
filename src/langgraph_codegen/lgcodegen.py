fb4097a1d3daf5ea4d21f75f6f2a0e6bfb3203d2
Joh Johannsen <johannes.johannsen@gmail.com>
2024-12-05 18:15:27 -0800

    fix copy-paste error in workflow

--- src/langgraph_codegen/lgcodegen.py
+++ src/langgraph_codegen/lgcodegen.py
@@ -19,7 +19,7 @@ from collections import namedtuple
 from typing_extensions import TypedDict
 
 # DO NOT EDIT, this is updated by runit script
-version="v0.1.36"
+version="v0.1.37"
 
 # Initialize colorama (needed for Windows)
 init()
